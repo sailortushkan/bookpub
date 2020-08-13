3bce3324b8246139b07be91578396b4f12f6cd70
Tatiana <sailortushkan@yandex.ru>
2020-08-13 21:22:19 +0500

    Airtable (#2)
    * [feature] implement book page and add airtable base

--- src/index.js
+++ src/index.js
@@ -1,1 +1,111 @@
-console.log('HELLOOOOOOOOOOO!');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import '../dist/index.css';
+
+class App extends Component {
+
+    constructor(props) {
+      super(props);
+      this.state = {
+          book: null,
+          author: null,
+      };
+    }
+
+    componentDidMount() {
+        fetch('https://api.airtable.com/v0/app8QPpFH2bwTTvxH/Books?api_key=keyXFiwBMjZtYhcLw')
+        .then((resp) => resp.json())
+        .then(data => {
+            this.setState({book: data.records[0].fields});
+        }).catch(err => {
+            // Error
+        });
+
+        fetch('https://api.airtable.com/v0/app8QPpFH2bwTTvxH/Authors?api_key=keyXFiwBMjZtYhcLw')
+        .then((resp) => resp.json())
+        .then(data => {
+            this.setState({ author: data.records[0].fields});
+        }).catch(err => {
+            // Error
+        });
+    }
+
+    render() {
+        return (
+            <BookCard {...this.state} />
+        )
+    }
+  }
+
+function BookCard(props) {
+    if (!props.book)
+        return <div className='loading'>Loading...</div>;
+    return (
+        <div className='book_card'>
+            <div className='column'>
+                <img src={props.book.coverUrl[0].url} alt={props.book.name} className='book_cover'></img>
+            </div>
+            <div className='column'>
+                <h1 className='book_title'>{props.book.name}</h1>
+                <p className='book_annotation'>{props.book.annotation}</p>
+                <p className='book_spec'>{props.book.pages} pages, in {props.book.language}</p>
+                <p className='book_spec'>{props.book.progress}% done</p>   
+                <p className='funding'>${props.book.sum_collected} out of ${props.book.sum_expected} collected!</p>            
+                <AuthorCard {...props.author} /> 
+                <div className='book_prices_container'>
+                    <div className='book_price'>
+                        <p>${props.book.price_minimum}</p>
+                        <p className='price_tag'>minimum price</p>
+                    </div>
+                    <div className='book_price'>
+                        <p>${props.book.price_suggested}</p>
+                        <p className='price_tag'>suggested price</p>
+                    </div>
+                </div>
+                
+                 <button className='grey_button'>Add to cart</button>
+            </div> 
+        </div>
+    )
+}
+
+function AuthorCard(props) {
+    if (!props.avatarUrl)
+        return <div>Loading...</div>;
+    return (
+        <div className='author_card'>
+            <img src={props.avatarUrl[0].url} alt={props.name} className='avatar'></img>
+            <div className='author_contacts'>
+                <p className='author_contacts_item'>{props.name}</p>
+                <p className='author_contacts_item'>{props.email}</p>
+                <p className='author_contacts_item'>{props.summary}</p>
+            </div>
+        </div>
+    )
+}
+
+
+
+let book = {
+    name: 'Walden. Life in the woods',
+    annotation: 'The text is a reflection upon simple living in natural surroundings. The work is part personal declaration of independence, social experiment, voyage of spiritual discovery, satire, and — to some degree — a manual for self-reliance.',
+    pages: 136,
+    language: 'English',
+    progress: 70,
+    coverUrl: './Walden.jpg',
+    author: {
+        name: 'Henry David Thoreau',
+        email: 'henry@thoreau.com',
+        avatarUrl: './Henry_David_Thoreau.jpg',
+        summary: 'Henry David Thoreau is an American essayist, poet, and philosopher-transcendentalist.'
+    },
+    price_minimum: 10,
+    price_suggested: 15,
+    sum_collected: 700,
+    sum_expected: 1000
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+  );
